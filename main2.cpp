7a42c7cdbbd0d632514db08e80dd1b0ad844baf3
rafaltym <rafaltym@wp.pl>
2017-10-02 17:21:05 +0200

    cwiczenie2

---
+++ main2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int w;
+    int b=1;
+    cout<<" podaj liczbe wierszy"<<endl;
+    cin>>w;
+
+    for(int i=0; i<=w;i++)
+    {
+        for(int j=0;j<w-i;j++)
+        {
+            cout<<" ";
+
+        }
+        for(int k=0; k<b;k++)
+        {
+            cout<<"x ";
+        }
+        b++;
+        cout<<endl;
+    }
+
+
+    return 0;
+}
+
